f9eb950c90900e47c0f216a84a5b591ddfabf2bb
andemar <andemartinez98@hotmail.com>
2019-11-14 11:29:50 -0500

    Tipos de datos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let c: number = 1;
+    let e: string = 'a';
+    let g: boolean = true;
+    let h: object = {};
+    let i = [c, c, e, g, h];
+    let j: boolean [] = [true, false];
+    let k: object[] = [{}, h];
+    let l: any[] = [1, 'asd', {}];
+
+    console.log(i);
+  }
 
   ngOnInit() {
   }
